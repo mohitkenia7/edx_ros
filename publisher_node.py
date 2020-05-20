3817c7c4dee92a470e310e99b0fafa75c8fbcf8e
mohitkenia7 <mohit.kenia7@gmail.com>
2020-05-20 09:08:14 +0000

    initial source commit

---
+++ publisher_node.py
@@ -0,0 +1,18 @@
+impirt rospy
+from std_msgs.msg import Strings
+
+def simplePublisher():
+	simple_publisher = rospy.Publisher('mohit', String, queue _size =10)
+	rospy.init_node('node-1', anonymous = False)
+	rate = rospy.Rate(10)
+
+	mohit_content = "my first ros topic"
+	while not rospy.is_shutdown():
+		simple_publisher.publish(mohit_content)
+		rate.sleep()
+
+if __namae__ = '__main__':
+	try:
+		simplePublisher()
+	except rospy.ROSInterruptException:
+		pass
